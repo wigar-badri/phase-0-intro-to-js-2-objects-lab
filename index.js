77f8ce30046a8e11670b5e4ec86fba1088c4b86f
wigarbadri <wigarbadri@gmail.com>
2023-07-04 09:22:22 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+// employee name and address 
+var employee = {
+    name: "John Doe",
+    streetAddress: "123 Main Street"
+  }
+  // Function to update an employee object with a new key-value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // Function to destructively update an employee object with a new key-value pair
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete a key-value pair from an employee object
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to destructively delete a key-value pair from an employee object
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
